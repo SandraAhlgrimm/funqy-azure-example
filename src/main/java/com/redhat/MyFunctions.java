891ef874bba708226c15023115fc0c02ef5e903e
jeremyrdavis <jeremyrdavis@mac.com>
2023-05-23 13:55:47 -0400

    building a -runner without a version name so that the host.json does not have to be updated at build time

--- src/main/java/com/redhat/MyFunctions.java
+++ src/main/java/com/redhat/MyFunctions.java
@@ -1,17 +1,39 @@
 package com.redhat;
 
 import io.quarkus.funqy.Funq;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MyFunctions {
 
-    @Funq
-    public String fun(FunInput input) {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MyFunctions.class);
+    @Funq("FunqyHttp")
+    public String fun(FunqyInput input) {
+
+        LOGGER.info("received: {}", input);
+
         return String.format("Hello %s!", input != null ? input.name : "Funqy");
     }
 
 
-    public static class FunInput {
+    public static class FunqyInput {
         public String name;
+
+        @Override
+        public String toString() {
+            return new StringBuilder("FunqyInput[name=")
+                    .append(name != null ? name : "''")
+                    .append("]")
+                    .toString();
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 
 }
