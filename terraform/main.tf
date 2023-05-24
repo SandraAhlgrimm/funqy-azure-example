2ac4acbceb57eb2673d4bba3d2734505c8c80be6
SandraAhlgrimm <sakriema@microsoft.com>
2023-05-24 20:02:58 +0200

    update functions to run on linux

--- terraform/main.tf
+++ terraform/main.tf
@@ -32,7 +32,7 @@ resource "azurerm_service_plan" "main" {
   resource_group_name = azurerm_resource_group.main.name
 
   sku_name = "Y1"
-  os_type  = "Windows"
+  os_type  = "Linux"
 }
 
 resource "azurerm_linux_function_app" "main" {
